8f5605b7a9e7989fbc83101e24c58754992ae030
Matt Potts <mattpotts@gmail.com>
2015-12-05 12:20:31 +0000

    Style dashboard to have massive buttons

--- rest/app/views/dashboard.php
+++ rest/app/views/dashboard.php
@@ -3,11 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            font-family: arial;
+        }
+
+        a.block {
+            display: block;
+            height: 50%;
+        }
+
+        .control {
+            text-decoration: none;
+            text-align: center;
+            /*display: table-cell;*/
+        }
+
+        /*.control > span {*/
+            /*vertical-align: middle;*/
+        /*}*/
+
+        .control-up {
+            background-color: #68ef5c;
+        }
+
+        .control-down {
+            background-color: #f62f2c;
+        }
+    </style>
 </head>
 <body>
 
-<a href="/1/up">Up</a>
-<a href="/1/down">Down</a>
+<a class="block control control-up" href="/1/up">
+<!--    <span>Up</span>-->
+<!--    Up-->
+</a>
+
+<a class="block control control-down" href="/1/down">
+<!--    <span>Down</span>-->
+<!--    Down-->
+</a>
 
 </body>
 </html>
