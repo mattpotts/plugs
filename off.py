0e54a4bf28ca46a13f0d64c2df5199b9063ce5a7
Matt Potts <mattpotts@gmail.com>
2015-12-05 01:24:16 +0000

    Add default scripts from various sources

---
+++ off.py
@@ -0,0 +1,24 @@
+from energenie import switch_on, switch_off
+from time import sleep
+
+# turn all plug sockets on and off
+switch_off(1)
+switch_off(2)
+switch_off(3)
+switch_off(4)
+#switch_off()
+#switch_on()
+
+# turn a plug socket on and off by number
+#switch_on(1)
+#switch_off(1)
+
+#switch_on(3)
+#switch_off(3)
+
+# turn some plug sockets on, then turn them off after 10 seconds
+#switch_on(1)
+#switch_on(4)
+#sleep(10)
+#switch_off(1)
+#switch_off(4)
